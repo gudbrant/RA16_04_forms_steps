1caa178ee0151f390796fb5822b2f20eea32b1ec
gudbrant <gudbrant@gmail.com>
2021-06-25 21:55:17 +0800

    Create WorkoutTable.jsx

---
+++ src/components/WorkoutTable.jsx
@@ -0,0 +1,31 @@
+function WorkoutTable({ workouts, handleEdit, handleDelete }) {
+  return (
+    <table className="ui basic table">
+      <thead>
+        <tr>
+          <th>Дата</th>
+          <th>Пройдено (км)</th>
+          <th>Действия</th>
+        </tr>
+      </thead>
+      <tbody>
+        {workouts.map(workout => (
+          <tr key={workout.id}>
+            <td>{workout.date}</td>
+            <td>{workout.distance}</td>
+            <td>
+              <button onClick={() => handleEdit(workout.id)}>
+                <i className="edit icon"></i>
+              </button>
+              <button onClick={() => handleDelete(workout.id)}>
+                <i className="trash icon"></i>
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default WorkoutTable;
